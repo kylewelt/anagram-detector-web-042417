6484952941dac6df580ee91e1737b7d722c0b941
github username <github email address>
2017-04-30 20:57:24 -0400

    created Anagram class and methods

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,34 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    matches = []
+
+    anagrams.map do |word|
+      # check if the word is an anagram of self.word
+
+      needle = self.word.split("")
+      haystack = word.split("")
+
+      sorted_needle  = needle.sort
+      sorted_haystack = haystack.sort
+
+      if sorted_needle == sorted_haystack
+        matches << word
+      end
+    end
+
+    # binding.pry
+
+    # return the matches
+    matches
+  end
+
+end
